f9296dcc9d9a793e53da79470aed9570ced0928d
minkminkk <mrminhlunvn1@gmail.com>
2024-03-28 15:31:19 +0700

    Update tests

--- tests/jobs/test_generate_daily_clickstream.py
+++ tests/jobs/test_generate_daily_clickstream.py
@@ -1,12 +1,14 @@
 import pytest
+
 from pyspark.sql import SparkSession, Row
-from pyspark.testing import assertDataFrameEqual
+from pyspark.sql.types import *
+from pyspark.testing import assertDataFrameEqual, assertSchemaEqual
 from faker import Faker
-from packages.faker_custom_providers import user_info
-from jobs.daily_clickstream_generation import generate_clickstream_to_df
+
+from jobs.daily_clickstream_generate import generate_clickstream_to_df
 
 
-@pytest.fixture(scope = "package")
+@pytest.fixture(scope = "package", autouse = True)
 def spark():
     spark = SparkSession.builder \
         .master("local") \
@@ -15,20 +17,45 @@ def spark():
     
     yield spark
 
-Faker.seed(0)
+@pytest.fixture(scope = "module")
+def fake():
+    fake = Faker()
+    Faker.seed(0)
 
-def test_generate_clickstream_to_df(spark: SparkSession):
-    df_user_track_ids = spark.createDataFrame(
-        [
-            Row(user_id = "23749021asdf834", track_id = "412875addddd"),
-            Row(user_id = "91asdf2857921835", track_id = "abcbad1412412"),
-            Row(user_id = "2579adf18357", track_id = "412875abcbc"),
-        ]
-    )
+    return fake
+
+
+def test_generate_clickstream_to_df(fake: Faker):
+    # Inputs
+    str_template = "^" * 22
+    map_user_track_ids = [
+        Row(
+            user_id = fake.hexify(str_template), 
+            track_ids = [fake.hexify(str_template) for _ in range(5)]
+        ) for _ in range(5)
+    ]
+    max_events_per_user = 2
     
+    # Call function
     df = generate_clickstream_to_df(
         "2018-01-01", "2018-01-02", 
-        df_user_track_ids, 2
+        map_user_track_ids, max_events_per_user
     )
 
-    assert df.count() == 2 * df_user_track_ids.count()
+    # Expected results
+    expected_schema = StructType(
+        [
+            StructField("event_id", StringType()),
+            StructField("event_ts", TimestampType()),
+            StructField("event_name", StringType()),
+            StructField("ipv4", StringType()),
+            StructField("user_id", StringType()),
+            StructField("user_agent", StringType()),
+            StructField("track_id", StringType())
+        ]
+    )
+    expected_length = max_events_per_user * len(map_user_track_ids)
+
+    # Test output
+    assertSchemaEqual(df.schema, expected_schema)
+    assert df.count() == expected_length
